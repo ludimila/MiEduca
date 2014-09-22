ec25832f4b6c092bfcadb8772c59ce88bb14b11f
Thiago Bernardes <tmb0710@gmail.com>
2014-09-22 09:06:37 -0300

    last commit

--- MiEduca/functional/GameEngine.hs
+++ MiEduca/functional/GameEngine.hs
@@ -9,20 +9,27 @@ import WordsTupleSetup
 
 manResistence = 6
 
+teste::Int->Int
+teste te = te+1
+
 gameLoop :: (String,String,BSTree content) -> Int -> IO ()
 gameLoop (hs,cr,(Node leaf left right)) i| i > manResistence = do
 		putStrLn "\n"
 		Interface.drawHangman i
                 putStrLn "\n Voce Perdeu =("
+		if left == Null || right == Null
+		then do putStrLn "O jogo acabou voce acertou x palavras"
+		else do
 		let gameCurrentLevel = BinarySearchTree.getCurrentNode left
 		let word = fst leaf
 		gameLoop ("",word,left) 0	
-		--gameLoop ("",cr,(Node leaf left right)) 0	
              	| all (`elem` hs) (map toLower cr) = do
                 putStrLn "\n "
                 Interface.printWord hs cr
                 putStrLn "\nParabesn voce ganhou"
-	--	gameLoop ("",cr,left) 0			
+		if left == Null || right == Null
+		then putStrLn "O jogo acabou voce acertou x palavras"
+		else do
 		let gameCurrentLevel = BinarySearchTree.getCurrentNode right
 		let word = fst gameCurrentLevel
 		gameLoop ("",word,right) 0	
--- MiEduca/functional/main.hs
+++ MiEduca/functional/main.hs
@@ -1,127 +1,18 @@
-module Hangman where
-
-import Data.Char
+import GameEngine
+import BinarySearchTree
+import WordsTupleSetup
 import System.IO
 
-w = "Haskell e legal" -- Provisorio
-maxI = 7
-
-hangman :: String -> Int -> IO ()
-hangman cs i | i > maxI = putStrLn "\nInfelizmente nao foi dessa vez"
-             | all (`elem` cs) (map toLower w) = do
-                putStrLn " "
-                printWord cs
-                putStrLn "\nVencedor "
-
-hangman cs i = do
-    putStrLn " "
-    drawHangman i
-    printWord cs
-    putStrLn "\nEnsira uma letra?"
-    c <- getChar >>= (return.toLower)
-    if (c `elem` (map toLower w)) then
-      hangman (c:cs) i 
-    else do
-      putStrLn $ "\n" ++ (show (i+1)) ++ " Erros\n"
-      hangman cs (i+1)
-
-printWord :: String -> IO ()
-printWord cs = mapM_ pC w 
-  where
-    pC x  | x == ' ' = putChar ' '
-          | toLower x `elem` cs = putChar x
-          | otherwise = putChar '_'
 
 main = do
-  hSetBuffering stdin NoBuffering
-  menu
-  hangman " " 0
+  	hSetBuffering stdin NoBuffering
+	let gameWords = WordsTupleSetup.wordsTuple
+	let gameLevelTree = BinarySearchTree.generateBSTreeFromList gameWords
+	let gameCurrentLevel = BinarySearchTree.getCurrentNode gameLevelTree
+	let word = (fst gameCurrentLevel)
+ 	gameLoop ("",word,gameLevelTree) 0
 
-menu :: IO ()
-menu = do
-  putStr "  +--+--+--+--+--+--+--+--+--+--+--+--+--+  \n"
-  putStr " |     Bem-vindo ao jogo da forca!        | \n"
-  putStr "  +--+--+--+--+--+--+--+--+--+--+--+--+--+  \n"
-  putStr "\n\n\n\n"
-  return ()
 
-drawHangman :: Int -> IO ()
-drawHangman 0 = do
-	putStr "   ___     \n"
-	putStr "  /   |    \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr " -+-       \n"
 
-drawHangman 1 = do
-	putStr "   ___     \n"
-	putStr "  /   |    \n"
-	putStr "  |   O    \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr " -+-       \n"
 
-drawHangman 2 = do
-	putStr "   ___     \n"
-	putStr "  /   |    \n"
-	putStr "  |   O    \n"
-	putStr "  |   |    \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr " -+-       \n"
 
-drawHangman 3 = do
-	putStr "   ___     \n"
-	putStr "  /   |    \n"
-	putStr "  |   O    \n"
-	putStr "  | --|    \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr " -+-       \n"
-
-drawHangman 4 = do
-	putStr "   ___     \n"
-	putStr "  /   |    \n"
-	putStr "  |   O    \n"
-	putStr "  | --|--  \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr " -+-       \n"
-
-drawHangman 5 = do
-	putStr "   ___     \n"
-	putStr "  /   |    \n"
-	putStr "  |   O    \n"
-	putStr "  | --|--  \n"
-	putStr "  |   |    \n"
-	putStr "  |        \n"
-	putStr "  |        \n"
-	putStr " -+-       \n"
-
-drawHangman 6 = do
-	putStr "   ___     \n"
-	putStr "  /   |    \n"
-	putStr "  |   O    \n"
-	putStr "  | --|--  \n"
-	putStr "  |   |    \n"
-	putStr "  |  /     \n"
-	putStr "  |        \n"
-	putStr " -+-       \n"
-
-drawHangman 7 = do
-	putStr "   ___     \n"
-	putStr "  /   |    \n"
-	putStr "  |   O    \n"
-	putStr "  | --|--  \n"
-	putStr "  |   |    \n"
-	putStr "  |  / \\  \n"
-	putStr "  |        \n"
-	putStr " -+-       \n"
