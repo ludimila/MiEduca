754cee65b7c2d7ab2145d92701b311c6cb2a7cb9
thiagotmb <tmb0710@gmail.com>
2014-11-17 11:22:50 -0200

    Created class Firefight

---
+++ MultiAgentes/src/Victim.java
@@ -0,0 +1,38 @@
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.core.AID;
+
+public class Victim extends Agent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void setup(){
+		
+		addBehaviour(new OneShotBehaviour(this){
+			
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			public void action(){
+				
+				ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+				message.addReceiver(new AID("Bombeiro",AID.ISLOCALNAME));
+				message.setLanguage("Português");
+				message.setOntology("Emergência");
+				message.setContent("Fogo");
+				myAgent.send(message);
+				
+			}
+				
+			
+		});
+		
+		
+	}
+}
