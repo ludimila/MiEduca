5d24fdbe8f40d826456676bc279fd2fce00272ae
Maria Luciene <marialucienefelix@gmail.com>
2014-11-18 09:44:39 -0200

    Registering Firefight in Yellow pages

--- MultiAgentes/src/agents/Firefight.java
+++ MultiAgentes/src/agents/Firefight.java
@@ -1,34 +1,69 @@
 package agents;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 
 public class Firefight extends Agent{
+	private static final long serialVersionUID = 1L;
 
 	protected void setup (){
+
+		ServiceDescription service = new ServiceDescription();
+		service.setType("Firefight");
+		service.setName("Combate a Incêndio");
 		
+		register(service);
+		getMessage();
+	
+	} 
+
+	protected void getMessage() {
 		addBehaviour(new CyclicBehaviour (this){
-			
+			private static final long serialVersionUID = 1L;
+
 			public void action (){
 				ACLMessage message = myAgent.receive();
 				if (message != null){
-				
+					ACLMessage  reply = message.createReply();
 					String content = message.getContent();
 					
 					if(content.equalsIgnoreCase("Fogo")){
-						
-						System.out.println("O cidadão " + message.getSender().getName()+"avisou um incendio");
+						reply.setPerformative(ACLMessage.INFORM);
+						reply.setContent("Recebi seu Aviso! Obrigada por Auxilia meu serviço");
+						myAgent.send(reply);
+						System.out.println("O cidadão " + message.getSender().getName()+"  avisou um incendio");
 						System.out.println("Vou ativar os procedimentos de combate a incendio");			
 						
 					}else
 						block();
-				}//fim do action
+				}// End action
 				
 			}
-		});//fim do behaviour
-	
+		});// End behaviour
+	}
+
+	protected void register(ServiceDescription service) {
+		DFAgentDescription dfAgentDescription = new DFAgentDescription();
+		dfAgentDescription.setName(getAID());
+		dfAgentDescription.addServices(service);
 		
+		// Registering agent in DF
+		try{
+			DFService.register(this,dfAgentDescription);
+		}catch(FIPAException fipaException){
+			fipaException.printStackTrace();
+		}
 	}
 	
-	
+	protected void takeDown() {
+		try{
+			DFService.deregister(this);
+		}catch(FIPAException fipaE){
+			fipaE.printStackTrace();
+		}
+	} 
 }
--- MultiAgentes/src/agents/Victim.java
+++ MultiAgentes/src/agents/Victim.java
@@ -3,22 +3,28 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
+import jade.util.leap.Iterator;
 import jade.core.AID;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class Victim extends Agent {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	protected void setup(){
 		
+		searchFirefight();
+		sendMessage();
+		receiveMessage();
+		
+	}
+
+	private void sendMessage() {
 		addBehaviour(new OneShotBehaviour(this){
 			
-			/**
-			 * 
-			 */
 			private static final long serialVersionUID = 1L;
 
 			public void action(){
@@ -29,27 +35,59 @@ public class Victim extends Agent {
 				message.setOntology("Emergência");
 				message.setContent("Fogo");
 				myAgent.send(message);
-				
 			}
-				
-			
 		});
-		
+	}
+
+	private void receiveMessage() {
 		addBehaviour(new CyclicBehaviour(this) {
 			
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public void action() {
 				ACLMessage message = myAgent.receive();
 				if(message != null){
 					String content = message.getContent();
-					
-					System.out.println("-->" + message.getSender().getName() + ":" + content);
+					System.out.println("-->" + message.getSender().getName() + " : " + content);
 				}else{
 					block();
 				}
-				
 			}
 		});
+	}
+
+	protected void searchFirefight() {
+		DFAgentDescription searchServices = new DFAgentDescription();
 		
+		ServiceDescription service = new ServiceDescription();
+		service.setType("Firefight");
+		
+		searchServices.addServices(service);
+		
+		try {
+			
+			DFAgentDescription[] firefight = DFService.search(this, searchServices);
+			printServices(firefight);
+			
+		}catch(FIPAException e){
+			e.printStackTrace();
+		}
+	}
+
+	private void printServices(DFAgentDescription[] firefight) {
+		
+		for(int i = 0; i < firefight.length; i++){
+			
+			String out = firefight[i].getName().getLocalName() + "  provê: ";
+			Iterator services = firefight[i].getAllServices();
+			
+			while(services.hasNext()){
+				ServiceDescription serviceDescription = (ServiceDescription) services.next();
+				out += " " + serviceDescription.getName();
+			}
+			
+			System.out.println(out);
+		}
 	}
 }
