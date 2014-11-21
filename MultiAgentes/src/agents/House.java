fd03703ab81775305a4712d8586272ac3a70a839
Maria Luciene <marialucienefelix@gmail.com>
2014-11-21 18:39:27 -0200

    ajustes finais

--- MultiAgentes/src/agents/House.java
+++ MultiAgentes/src/agents/House.java
@@ -13,7 +13,7 @@ public class House extends Agent {
 
 	private static final long serialVersionUID = 1L;
 
-	private static final int SIZE = 10;
+	private static final int SIZE = 3;
 	public static AgentController victimAgent;
 	public static AgentController firefightAgent;
 	public static final int VICTIM = 2;
@@ -70,14 +70,18 @@ public class House extends Agent {
 		}
 
 		private void setFirePosition() {
+
 			boolean victimSave = setFirefightLocation();
 
 			for (int countLine = 0; countLine < house.length; countLine++) {
 				for (int countColumn = 0; countColumn < house[countLine].length; countColumn++) {
+					
+					System.out.println("Fogo posicao:  Vetor posição " + house[countLine][countColumn]);
+					System.out.println("Fogo posição da vitima" + VICTIM);
+					
 
-					if (house[countLine][countColumn] == VICTIM && victimSave == false) {
-						System.out.println("Posicao do fogo:  ( X = "
-								+ countLine + "  Y= " + countLine + ")");
+					if (house[countLine][countColumn] == VICTIM) {
+						System.out.println("Posicao do fogo:  ( X = " + countLine + "  Y= " + countLine + ")");
 						System.out.println("Vitima alcancada pelo fogo");
 					} else {
 						house[countLine][countColumn] = FIRE;
@@ -97,19 +101,8 @@ public class House extends Agent {
 			x = random.nextInt(SIZE);
 			y = random.nextInt(SIZE);
 
-			try {
-
-				if (victimAgent.getName().equals(
-						"Vitima@192.168.25.16:1099/JADE")) {
-
-					System.out.println("Posicao da vitima: (X= " + x + "  Y= "
-							+ y);
-					house[x][y] = VICTIM;
-				}
-
-			} catch (StaleProxyException e) {
-				e.printStackTrace();
-			}
+			System.out.println("Posicao da vitima: (X= " + x + "  Y= " + y + ")");
+			house[x][y] = VICTIM;
 
 		}
 
@@ -121,33 +114,26 @@ public class House extends Agent {
 			int line = 0;
 			int column = 0;
 
-			line = random.nextInt(SIZE);
-			column = random.nextInt(SIZE);
+			line = random.nextInt(SIZE);       	System.out.println("Bombeiro randomico X: " +  line);
+			column = random.nextInt(SIZE);         System.out.println("Bombeiro colum Y: " + column);
 
-			
-			try {
+			for (int countLine = line; countLine < house.length; countLine++) {
+				for (int countColumn = column; countColumn < house[countLine].length; countColumn++) {
 
-				if (firefightAgent.getName().equals(
-						"Bombeiro@192.168.25.16:1099/JADE")) {
+					System.out.println("Posiçãodo bombeiro ( X = " + countLine + "  Y= " + countLine + ")");
 					
-					for (int countLine = line; countLine <= SIZE; countLine++) {
-						for (int countColumn = column; countColumn <= SIZE; countColumn++) {
-					
-							if (house[countLine][countColumn] == VICTIM) {
-								System.out.println("Posicao do bombeiro:  ( X = "+ countLine + "  Y= " + countLine + ")");
-								System.out.println("Vitima Salva");
-								isSave = true;
-							} else {
-								house[countLine][countColumn] = FIREFIGHT;
-							}
-						}
-					}
+					/*
+					if (house[countLine][countColumn] == VICTIM) {
+						System.out.println("Posicao do bombeiro:  ( X = " + countLine + "  Y= " + countLine + ")");
+						System.out.println("Vitima Salva");
+						isSave = true;
+					} else {
+						house[countLine][countColumn] = FIREFIGHT;
+					} */
 				}
-
-			} catch (StaleProxyException e) {
-				e.printStackTrace();
 			}
+
 			return isSave;
 		}
-	}   
+	}
 }
